ed964913acc17e078f2d583ed75a6653744922ad
Alexander Auritt <alexauritt@gmail.com>
2011-04-22 21:35:41 -0400

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pedalto::Application.config.secret_token = 'aa02dc420eafd776a8b8e0104f74c672e8bebf8112444164d15cd131ed7a486046ed373161e8315ab52cf6428cc4e38351254c6a9d98ab7216a7b2635a1a8897'
